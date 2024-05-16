d7e298d02b6c22af11db1b15c47377bc6f423d3a
Philippe Beck <philippe@philippebeck.net>
2024-05-16 12:17:38 +0400

    Add hgroup & subtitle to title atom

--- src/components/atoms/title/title.jsx
+++ src/components/atoms/title/title.jsx
@@ -1,14 +1,21 @@
 import "./title.css"
 
-const Title = ({ name, lvl="2" }) => {
+const Title = ({ title, lvl="2", subtitle="" }) => {
 
   return (
-    lvl === "1" ? <h1>{name}</h1> :
-    lvl === "2" ? <h2>{name}</h2> :
-    lvl === "3" ? <h3>{name}</h3> :
-    lvl === "4" ? <h4>{name}</h4> :
-    lvl === "5" ? <h5>{name}</h5> :
-    <h6>{name}</h6>
+    <hgroup>
+      {
+        lvl === "1" ? <h1>{title}</h1> :
+        lvl === "2" ? <h2>{title}</h2> :
+        lvl === "3" ? <h3>{title}</h3> :
+        lvl === "4" ? <h4>{title}</h4> :
+        lvl === "5" ? <h5>{title}</h5> :
+        <h6>{title}</h6> 
+      }
+      {
+        subtitle && <p>{subtitle}</p>
+      }
+    </hgroup>
   )
 }
 
